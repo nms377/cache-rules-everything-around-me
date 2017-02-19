2fe9daecb1b5fbb1f2b928039407a2cc0e92dd2e
nms377 <nms377@nyu.edu>
2017-02-18 19:10:59 -1000

    Moved my cache route out of slow.js and into the server.

--- routes/slow.js
+++ routes/slow.js
@@ -3,12 +3,10 @@ const express = require('express');
 
 const sleep = require('../services/sleep');
 
-const cache = require('express-redis-cache')();
-
 const router = express.Router();
 
 router.route('/')
-  .get(cache.route('/', 60), (req, res, next) => {
+  .get((req, res, next) => {
     return sleep(5000)
       .then(_ => res.render('api/index', (err, html) => {
         res.send(html);
