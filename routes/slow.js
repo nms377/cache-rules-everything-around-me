397e07a884344854e1edd0679f8ad123021d0668
nms377 <nms377@nyu.edu>
2017-02-18 18:20:55 -1000

    Moved code into slow.js file. Put cache.route in the get request and put an expiration of 60 seconds.

--- routes/slow.js
+++ routes/slow.js
@@ -3,10 +3,12 @@ const express = require('express');
 
 const sleep = require('../services/sleep');
 
+const cache = require('express-redis-cache')();
+
 const router = express.Router();
 
 router.route('/')
-  .get((req, res, next) => {
+  .get(cache.route('/', 60), (req, res, next) => {
     return sleep(5000)
       .then(_ => res.render('api/index', (err, html) => {
         res.send(html);
